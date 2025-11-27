e3c88dd854e54d762139d00c595049f5ccd66a84
Aswinrajr <aswinrajr07@gmail.com>
2025-11-27 19:17:58 +0530

    backend check

--- draftmaker-backend/routes/ccavenueRoutes.js
+++ draftmaker-backend/routes/ccavenueRoutes.js
@@ -8,4 +8,8 @@ paymentRouter.post('/initiate-payment', initiatePayment);
 // Handle CCAvenue response (both success and cancel redirects here)
 paymentRouter.post('/response', handleResponse);
 
+paymentRouter.get('/response', (req, res) => {
+    return res.status(405).send("This route only accepts POST from CCAvenue");
+});
+
 module.exports = paymentRouter;
