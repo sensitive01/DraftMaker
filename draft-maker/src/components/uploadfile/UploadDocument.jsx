fd364ed39fe3b4ec8175b4689c7ed97be4dba7f5
Aswinrajr <aswinrajr07@gmail.com>
2025-11-29 13:32:34 +0530

    check the env

--- draft-maker/src/components/uploadfile/UploadDocument.jsx
+++ draft-maker/src/components/uploadfile/UploadDocument.jsx
@@ -662,7 +662,9 @@ const DocumentUpload = () => {
       // Submit form to CCAvenue
       const form = document.createElement("form");
       form.method = "POST";
-      form.action = "https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction";
+      form.action =
+        "https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction";
+      // form.action = "https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction";
 
       form.innerHTML = `
       <input type="hidden" name="encRequest" value="${data.encRequest}" />
